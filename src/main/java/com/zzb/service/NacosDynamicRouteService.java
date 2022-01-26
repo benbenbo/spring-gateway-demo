541e4cf5c935c7700f31966ef3013df923d8eff0
zhengbingyuan <zhengbingyuan@leyopharm.com>
2022-01-26 15:58:45 +0800

    feat(gateway功能): 实现简单转发

---
+++ src/main/java/com/zzb/service/NacosDynamicRouteService.java
@@ -0,0 +1,11 @@
+package com.zzb.service;
+
+import org.springframework.context.ApplicationEventPublisherAware;
+
+/**
+ * @author zhengbingyuan
+ * @date 2022/1/26
+ */
+public class NacosDynamicRouteService{
+
+}
